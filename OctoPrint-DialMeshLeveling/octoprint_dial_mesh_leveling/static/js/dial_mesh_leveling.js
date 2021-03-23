69e6c4793249f531db9197c69dea35febe56de57
Anchor Huang <anchor.huang@gmail.com>
2021-03-23 22:53:20 +0800

    update tab

--- OctoPrint-DialMeshLeveling/octoprint_dial_mesh_leveling/static/js/dial_mesh_leveling.js
+++ OctoPrint-DialMeshLeveling/octoprint_dial_mesh_leveling/static/js/dial_mesh_leveling.js
@@ -13,6 +13,17 @@ $(function () {
         // self.settingsViewModel = parameters[1];
 
         // TODO: Implement your plugin's view model here.
+        self.mesh_data = ko.observableArray();
+        self.dial_value = ko.observable();
+
+        self.getDialValue = function() {
+            OctoPrint.get("plugin/dial_mesh_leveling/get_dail_value")
+                .done(function(response) {
+                    self.dial_value(response.value);
+            });
+
+        };
+
     }
 
     /* view model class, parameters for constructor, container to bind to
@@ -27,6 +38,7 @@ $(function () {
         ],
         // Elements to bind to, e.g. #settings_plugin_dial_mesh_leveling, #tab_plugin_dial_mesh_leveling, ...
         elements: [
+            "#tab_plugin_dial_mesh_leveling"
             /* ... */
         ]
     });
